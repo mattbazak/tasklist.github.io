040143f8e2b58f2a1aa8febd4e3ba840bebbd0da
mattbazak <matt@banhmi.dev>
2021-02-05 17:51:19 +0000

    update

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // add task events
   form.addEventListener('submit', addTask);
+  //remove task event
+  taskList.addEventListener('click', removeTask);
+  //clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 
@@ -43,4 +49,41 @@ function addTask(e) {
 
 
   e.preventDefault();
+}
+
+
+//Remove Task
+
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+   if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+    }
+  }
+}
+
+//Clear Tasks
+
+function clearTasks() {
+  if(confirm('Remove all tasks?')) {
+    // taskList.innerHTML = '';
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+}
+
+//Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLocaleLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  });
+
 }
